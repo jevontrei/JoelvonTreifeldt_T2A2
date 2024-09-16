9c4a98e0a93236b160030c2c32e34facc2e47b04
jevontrei <160815182+jevontrei@users.noreply.github.com>
2024-09-16 12:03:06 +1000

    build log update controller

--- src/controllers/log_controller.py
+++ src/controllers/log_controller.py
@@ -1,6 +1,7 @@
 from init import db
 from main import app
 from models.models import Log, log_schema, logs_schema
+from flask import request
 
 ############################################
 
@@ -31,7 +32,6 @@ def get_patient_log(patient_id):
     return logs_schema.dump(logs)
 
 
-
 ############################################
 
 @app.route("/logs/<int:log_id>", methods=["DELETE"])
@@ -42,6 +42,46 @@ def delete_log(log_id):
     if log:
         db.session.delete(log)
         db.session.commit()
-        return {"message": "Log {log_id} deleted."}  # , 200
+        return {"message": f"Log {log_id} deleted."}  # , 200
     else:
-        return {"error": "Sorry, log {log_id} can't be found."}  # , 404?
+        return {"error": f"Sorry, log {log_id} can't be found."}  # , 404?
+
+############################################
+
+# @app.route("/logs/", methods=["POST"])
+# def create_log():
+#     body_data = request.get_json()
+#     stmt = db.select(Log).filter_by(log_id=log_id)
+#     log = db.session.scalar(stmt)
+    
+#     if log:
+#         log = Log(
+#             ...
+#         )
+
+#         db.session.add(log)
+#         db.session.commit()
+
+#         return log_schema.dump(log)  # , 201
+
+#     else:
+#         return {"error": f"Log {log_id} not found."}
+    
+############################################
+
+# not sure if update route should be /logs/ or /logs/log_id
+@app.route("/logs/<int:log_id>", methods=["PUT", "PATCH"])
+def update_log(log_id): 
+    body_data = request.get_json()
+    stmt = db.select(Log).filter_by(log_id=log_id)
+    log = db.session.scalar(stmt)
+    if log:
+        log.date = body_data.get("date") or log.date
+        log.symptom = body_data.get("symptom") or log.symptom
+        log.duration = body_data.get("duration") or log.duration
+        log.severity = body_data.get("severity") or log.severity
+        # Do it for FK too? Probably not. A log is not realistically going to change patients
+        db.session.commit()
+        return log_schema.dump(log)
+    else:
+        return {"error": f"Log {log_id} not found."}, 404
